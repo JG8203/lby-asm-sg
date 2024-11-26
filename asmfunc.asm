5e039d89043bfbe79d6ff61dd1998095ea445730
Joshua Armaine Gilo <joshuaarmainegerzangilo@gmail.com>
2024-11-26 09:42:05 +0800

    Prepared assembly code.

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,15 +1,29 @@
 section .data
-    msg db "Hello World", 13, 10, 0  ; Message with carriage return, line feed, and null terminator
+    width_val dq 0
+    height_val dq 0
+    pixels_ptr dq 0
+    converted_ptr dq 0
 
 section .text
 bits 64
-default rel                          ; to handle address relocation
-global asmhello                      ; make the function visible to linker
-extern printf                        ; declare the C function we'll call
+default rel
+global imgCvtGrayIntToDouble
 
-asmhello:
-    sub rsp, 8*5                     ; allocate shadow space for Windows x64 calling convention
-    lea rcx, [msg]                   ; first parameter for printf
-    call printf                      ; call C printf function
-    add rsp, 8*5                     ; restore stack
+imgCvtGrayIntToDouble:
+
+    ;; In asm, parameters are passed as the ff:
+    ;; func(rcx, rdx, r8, r9, [stack])
+    ;; where the first parameter is passed onto rcx, the second onto rdx,
+    ;; the third onto r8, and the fourth onto r9.
+    ;; Any additional parameters (5th, 6th, etc.) are passed on the stack.
+
+    ;; Demo code for moving around the params
+    mov [width_val], rcx ;; stores first parameter into width_val
+    mov [height_val], rdx ;; stores second parameter into height_val
+    mov al, byte [r8] ;; stores character in source array (index 0) to al
+    mov [r9], al ;; stores character in al to index 0 of dest array
     ret
+
+    ;; TODO:
+    ;; Use XMM to store floating point values and store into r9
+    ;; Use SIMD instructions to do the conversion from source to dest array
